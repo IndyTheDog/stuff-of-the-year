8e2e3bf15aa42dd89f69a99fe2463e051f3e7a4d
Cédric Rochefolle <85271877+IndyTheDog@users.noreply.github.com>
2023-06-29 13:01:27 +0700

    Fix test data

--- app/components/Message.cy.tsx
+++ app/components/Message.cy.tsx
@@ -1,19 +1,20 @@
 import DialogData from '../models/DialogData'
+import VoteData from '../models/VoteData'
 import Message from './Message'
 
 const dialogData: DialogData = {
   controlA: 'Yes',
   controlB: 'No',
-  voteStepTwo: (choice: boolean) => void(choice),
+  voteStepTwo: (data: VoteData) => void data,
   message: 'Thank you for your vote',
   title: 'Thank you',
   open: true,
+  data: { id: 0, name: '' },
 }
-
 
 describe('<Message />', () => {
   it('should not be visible if open is false', () => {
-    const closedDialogData = {...dialogData}
+    const closedDialogData = { ...dialogData }
     closedDialogData.open = false
     cy.mount(<Message data={closedDialogData} />)
 
@@ -25,22 +26,25 @@ describe('<Message />', () => {
     cy.get('dialog').should('be.visible')
   })
 
-
   it('should execute action if button is clicked', () => {
     const mock = {
       voteStepTwo(choice: boolean) {
-        void(choice)
+        void choice
       },
     }
     const spy = cy.spy(mock, 'voteStepTwo')
-    const newDialogData = {...dialogData}
+    const newDialogData = { ...dialogData }
     newDialogData.voteStepTwo = spy
 
     cy.mount(<Message data={newDialogData} />)
     cy.get('dialog').should('be.visible')
-    cy.get('dialog').find('button').first().click().then(() => {
-      expect(spy).to.have.been.called
-    })
+    cy.get('dialog')
+      .find('button')
+      .first()
+      .click()
+      .then(() => {
+        expect(spy).to.have.been.called
+      })
   })
 })
 
